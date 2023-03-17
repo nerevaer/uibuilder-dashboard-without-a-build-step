65586b5e7965753402d502f629b4438430b416f8
nerevaer <87225499+nerevaer@users.noreply.github.com>
2023-03-17 14:51:56 +0100

    Add files via upload

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,25 +1,59 @@
 <template>
     <div class="dashboard">
-        <router-view></router-view>
-            <!--<Sidebar/>-->   
+        <!--<router-view></router-view>-->
+        <Sidebar></Sidebar>
         <div class="content">
-            <router-view name="Sidebar"></router-view>
-            
-            <!--<router-view/>-->   
+            <router-view></router-view>
+            <!--<router-view name="Sidebar"></router-view>-->
         </div>
     </div>
 </template>
 
-
-
-
-
+<script>
+const Sidebar = httpVueLoader('../components/Sidebar.vue');
+const Messages = httpVueLoader('./Messages.vue');
+const Overview = httpVueLoader('./Overview.vue');
+/*const routeDefs = [{
+        path: '',
+        component: Sidebar,
+        children: [{
+            path: '/',
+            components: Overview,
+          },{
+            path: '/messages',
+            components: Messages,
+}]}]*/
+const routeDefs = [
+    {
+        path: '/',
+        component: Sidebar
+    },
+    {
+        path: '/overview',
+        component: Overview
+    },{
+        path: '/messages',
+        component: Messages
+    }
+]
+module.exports = {
+    /** Allow greeting and who to be passed as parameters but provide defaults
+     * @see https://vuejs.org/v2/guide/components-props.html#Prop-Validation
+     */
+    router: new VueRouter({routes: routeDefs}),
+    components: {
+        'Sidebar': Sidebar,
+        'Overview': Overview,
+        'Messages': Messages
+    }
+}
+</script>
 
 <style>
 .dashboard {
     display: grid;
     grid-template-columns: 1fr 5fr;
-    background-color: rgba(13, 0, 128, 0.26);
+    background-color: rgba(17, 10, 82, 0.719);
     height: 100vh;
     width: 100vw;
 }
